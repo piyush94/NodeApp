dd6c56b58666e6819d645f11699e75f208fb9952
piyush94 <piyush.sohal94@gmail.com>
2018-02-21 15:51:12 +0530

    url module

--- Tutorial/main.js
+++ Tutorial/main.js
@@ -1,7 +1,9 @@
 var http = require('http');
-var myDateTime = require('./myDateTime');
+var url = require('url');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end("The date and time are currently: " + myDateTime.myDateTime() + "<br/><br/>" + req.url);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
 }).listen(8080);
